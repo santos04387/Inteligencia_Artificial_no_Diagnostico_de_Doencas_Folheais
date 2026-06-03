ac99c82d30fbc937441a78e0712186612fc02d21
santos04387 <kettlyn.correa@escola.pr.gov.br>
2026-06-03 08:49:10 -0300

    s

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
  const menuToggle = document.getElementById('menu-toggle');
 const navbar = document.getElementById('navbar');
 
-// Escuta o clique no botão das 3 barras
+// Escuta o clique nas 3 barras do menu
 menuToggle.addEventListener('click', () => {
-    // Abre ou fecha a janela com a lista de links
     navbar.classList.toggle('active');
-    
-    // Altera a classe CSS do botão para transformá-lo visualmente em um 'X'
     menuToggle.classList.toggle('open');
 });
 
-// Fecha o menu de forma automática ao clicar em qualquer link (melhora o uso no smartphone)
+// Fecha o menu de forma automática ao clicar em qualquer link
 const navLinks = document.querySelectorAll('#navbar a');
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
