c1a1354c82d70d0f4277302a93db7e58e6cb9fe5
santos04387 <kettlyn.correa@escola.pr.gov.br>
2026-06-10 08:52:09 -0300

    S

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 function toggleMenu() {
     let menu = document.getElementById("menu");
-
-    if (menu.style.left === "0px") {
-        menu.style.left = "-250px";
-    } else {
-        menu.style.left = "0px";
-    }
+    menu.style.left = (menu.style.left === "0px") ? "-250px" : "0px";
 }
+
+function toggleDarkMode() {
+    document.body.classList.toggle("dark");
+}
+
+function topo() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+/* ANIMAÇÃO SCROLL */
+window.addEventListener("scroll", () => {
+    let elements = document.querySelectorAll(".reveal");
+
+    elements.forEach(el => {
+        let pos = el.getBoundingClientRect().top;
+        let windowHeight = window.innerHeight;
+
+        if (pos < windowHeight - 100) {
+            el.classList.add("active");
+        }
+    });
+});
